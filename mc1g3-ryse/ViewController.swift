a4f0d2532a7fbdd5bd2e018f765376b98f3d7415
Griffin <griffin.seannery@yahoo.com>
2020-04-03 15:18:57 +0700

    tes2

--- mc1g3-ryse/ViewController.swift
+++ mc1g3-ryse/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        //hohohoho
         //tes ngepush - Griffin
     }
 
