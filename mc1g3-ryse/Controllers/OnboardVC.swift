09337aed4a48e13dfc19fe365f85bfcc92855272
Derry <derryantonio13@gmail.com>
2020-04-10 23:43:59 +0700

    Onboard added, asset updated

---
+++ mc1g3-ryse/Controllers/OnboardVC.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardVC.swift
+//  mc1g3-ryse
+//
+//  Created by derry on 10/04/20.
+//  Copyright © 2020 Grace Cindy. All rights reserved.
+//
+
+import UIKit
+
+class OnboardVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
