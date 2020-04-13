e2b2e1f17d672365005679787f0cde55cda81fa6
Derry <derryantonio13@gmail.com>
2020-04-13 15:34:22 +0700

    Onboard completed.

--- mc1g3-ryse/Controllers/OnboardVC.swift
+++ mc1g3-ryse/Controllers/OnboardVC.swift
@@ -8,22 +8,29 @@
 
 import UIKit
 
-class OnboardVC: UIViewController {
-
+class OnboardVC: UIViewController, UINavigationControllerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+       
+    }
+        
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    @IBAction func skipAction(_ sender: UIButton) {
+        UserDefaults.standard.set(true, forKey: "isAppAlreadyLaunchedOnce")
+        UserDefaults.standard.synchronize()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func readyAction(_ sender: UIButton) {
+        UserDefaults.standard.set(true, forKey: "isAppAlreadyLaunchedOnce")
+        UserDefaults.standard.synchronize()
     }
-    */
-
 }
